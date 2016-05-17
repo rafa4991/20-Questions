40ec871b30185d74cd4774819642aa7363ea6c0b
rafa4991 <rafa.perez720@gmail.com>
2016-05-16 18:27:22 -0700

    Game and Controller classes added

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,37 @@
+//Class that handles individual games and their components
+
+import java.util.*;
 
 public class Game {
+	private String id = "";
+	private ArrayList<Player> players = new ArrayList<Player>();
+	private ArrayList<String> history = new ArrayList<String>();
+	
+	//Constructor
+	public Game(String id){
+		this.id = id;
+	}
+	
+	//GET Methods
+	public String getId(){
+		return id;
+	}
+	public ArrayList<Player> getPlayers(){
+		return players;
+	}
+	public ArrayList<String> getHistory(){
+		return history;
+	}
+	
+	//SET Methods
+	public void addPlayer(Player player){
+		if(players.size()< 2){
+			players.add(player);
+		}
+	}
+	public void addHistory(String history){
+		this.history.add(history);
+	}
+	
 
 }
